3cc8eb87183317d98a0656ea33b14c1d52f50a00
unknown <ahmedirheem2003@gmail.com>
2023-08-17 12:19:11 +0300

    layout

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+import "@styles/globals.css";
+
+import Navbar from "@components/Navbar";
+import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <head >
+      <link rel="icon" href="/assets/images/logo.svg" />
+    </head >
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Navbar />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
+
